63b6c3f4dafb2b65b9283e17008d9bbd7dd18f63
EkaterinaColleague <paranina_em@mai.ru>
2024-06-21 16:06:52 +0400

    updated test

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,23 +3,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class MyFirstTest
-{
-   @Test
-   public void searchElements(){
-       //1.открыть браузер
-       //1.зайти на сайт
-       System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-       WebDriver browser = new ChromeDriver();
-
-       browser.get("https://www.saucedemo.com/");
-       browser.findElement(By.id("user-name"));
-       browser.findElement(By.name("login-button"));
-       browser.findElement(By.cssSelector("login-box"));
-       browser.findElement(By.cssSelector("#login-button"));
-       browser.findElement (By.xpath("//tag[contains(text(),'performance_glitch_user')]");
-       browser.findElement (By.xpath( "//button[contains(text), 'Login')]");
-       browser.quit();
-   }
-
+public class MyFirstTest {
+    @Test
+    public void searchElements() {
+        //1.открыть браузер
+        //1.зайти на сайт
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.id("user-name"));
+        browser.findElement(By.name("login-button"));
+        browser.findElement(By.cssSelector("login-box"));
+        browser.findElement(By.cssSelector("#login-button"));
+        browser.findElement(By.xpath("//div[contains (@class), 'login_container']"));
+        browser.findElement(By.xpath("//button[contains(text), 'Login')]"));
+        browser.quit();
+    }
+}
 
