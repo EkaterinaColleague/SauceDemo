d0138424014e89fa18a1bd6f04ce1ff9872b615a
Ekaterina Paranina <paranina_em@mail.ru>
2024-06-27 08:52:49 +0400

    Update LoginTest.java

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -10,18 +10,21 @@ public class LoginTest  extends BaseTest{
         loginPage.login("standard_user","secret_sauce");
         assertEquals(driver.findElement(By.xpath("//*[@class= 'title']")).getText(), "Products");
     }
+    
     @Test
     public void loginNoPassword() {
         loginPage.open();
         loginPage.login("standard_user","");
         assertEquals(driver.findElement(By.xpath("//*[@class= 'error-message-container error']")).getText(), "Epic sadface: Password is required");
     }
+    
     @Test
     public void loginFalse() {
         loginPage.open();
         loginPage.login("Katy_user","secret_sauce");
         assertEquals(driver.findElement(By.xpath("//*[@class= 'error-message-container error']")).getText(), "Epic sadface: Username and password do not match any user in this service");
     }
+    
     @Test
     public void loginWrongPassword() {
         loginPage.open();
